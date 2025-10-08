bd55da11e5a04de8b9ce9d75a9925848091cd4bf
Ganesh Chandrasekaran <gchandra@gmail.com>
2025-10-08 11:45:21 -0400

    Version 1.1

--- 01_json.py
+++ 01_json.py
@@ -2,8 +2,15 @@ from friends_data import friends_json_data
 import json
 
 obj_data = friends_json_data()
+
 friends_data = obj_data.simple_data().get("characters")
 
+friends_nested_data = obj_data.nested_data()
+
+
 # Serialize to JSON
-with open("data/friends.json", "w") as json_file:
+with open("data/friends_data.json", "w") as json_file:
     json.dump(friends_data, json_file)
+    
+with open("data/friends_nested.json", "w") as json_file2:
+    json.dump(friends_nested_data, json_file2)
